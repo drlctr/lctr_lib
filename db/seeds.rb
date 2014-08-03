6bd1a6fa743fa3bac4bc65b1cd3aaf1af7722039
Neil Woodward <drlctr@gmail.com>
2014-08-03 13:36:25 -0700

    More tweaks to Foundation. [#76105868]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+books = Book.create([
+	{title: 'POODR', author: 'Metz', ISBN: '2222'}
+	])
