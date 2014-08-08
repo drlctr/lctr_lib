10e395613721fe9b3c9cc233e6f5e80b05ac4a12
Neil Woodward <drlctr@gmail.com>
2014-08-07 23:32:58 -0700

    Added final validations to User model, [Finishes #76498742]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
 
-	validates_presence_of :username, :email
-	validates :password, confirmation:  true
+	validates_presence_of :username
+	validates :email, format: 
+	  {with: /\A.+@.+\..+\z/, message:
+	    "Does not appear to be a valid email address"}
+	validates :password, confirmation:  true, format:  
+	  {with: /\A(?=.*(\d|\W)).{5,20}\z/, message: "requires at least eight
+	   characters, at least one number or non-alphanumeric"}
 
 	has_many :books
 	
