5ee77364d234b8dadb0a56a3ff15a502c6810746
Neil Woodward <drlctr@gmail.com>
2014-08-07 23:08:45 -0700

    Added password conf logic to User model -- need to add more validation to password. [#76498742]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,8 +69,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.permit(:username, :email, :password, :password_confirmation)
-      # params[:user]
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
 end
