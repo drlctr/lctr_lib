2c6e4ab4b67e2387e5957f37c076f6e203067988
Neil Woodward <drlctr@gmail.com>
2014-08-10 00:25:10 -0700

    Added necessity to authenticate before messing with Books.  [finishes #76104502, #76104504]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
 	validates_presence_of :username
-	validates :email, format: 
-	  {with: /\A.+@.+\..+\z/, message:
-	    "Does not appear to be a valid email address"}
+	# validates :email, format: 
+	#   {with: /\A.+@.+\..+\z/, message:
+	#     "Does not appear to be a valid email address"}
 	validates :password, confirmation:  true, format:  
-	  {with: /\A(?=.*(\d|\W)).{5,20}\z/, message: "requires at least eight
+	  {with: /\A(?=.*(\d|\W)).{8,20}\z/, message: "requires at least eight
 	   characters, at least one number or non-alphanumeric"}
 
 	has_many :books
