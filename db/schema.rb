a2a04f42a1e97c3b791f0e6a282273a68d7f238e
Neil Woodward <drlctr@gmail.com>
2014-08-13 23:26:40 -0700

    Added rolify and cancancan gems; configured rolify [#76928014]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812054431) do
+ActiveRecord::Schema.define(version: 20140814055810) do
 
   create_table "average_caches", force: true do |t|
     t.integer  "rater_id"
@@ -72,6 +72,17 @@ ActiveRecord::Schema.define(version: 20140812054431) do
     t.integer  "user_id"
   end
 
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], name: "index_roles_on_name"
+
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "email"
@@ -91,6 +102,15 @@ ActiveRecord::Schema.define(version: 20140812054431) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_roles", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
 
   create_table "views", force: true do |t|
     t.string   "email",                  default: "", null: false
