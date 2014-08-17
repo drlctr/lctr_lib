1d61df952384021ad76a88e63f9b53bae3ab8467
Neil Woodward <drlctr@gmail.com>
2014-08-17 13:57:10 -0700

    Added guest accounts under cancan [#76104502]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,28 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
   	redirect_to root_url, alert: exception.message
   end
-  
+
+  def current_user
+   super || guest_user
+  end
+
   protected
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up).push(:username, :email)
   end
 
+  private
+
+  def guest_user
+   User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
+  end
+
+  def create_guest_user
+    u = User.create(username: "guest", 
+      email: "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+    u.save(validate: false)
+    u
+  end
+  
 end
