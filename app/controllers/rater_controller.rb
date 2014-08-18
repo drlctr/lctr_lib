38edae78f8866c845836a5f81c62458114c19811
Neil Woodward <drlctr@gmail.com>
2014-08-17 20:37:12 -0700

    Added email to book owner when book is rated [finishes #76104686]

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -6,6 +6,8 @@ class RaterController < ApplicationController
       obj.rate params[:score].to_f, current_user, params[:dimension]
 
       render :json => true
+      puts "id = #{params[:id]}, stars = #{params[:score]}, user = #{current_user.username}"
+      AdminMailer.notify_about_rating(params[:id],params[:score],current_user).deliver
     else
       render :json => false
     end
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,19 @@ class AdminMailer < ActionMailer::Base
   def notify_about_review(book, review)
   	@book = book
   	@review = review
-  	mail(to: @book.user.email, subject: "Your book, #{@book.title} was reviewed!")
+  	if @book.user.user_profile.mail_pref_rev
+  	  mail(to: @book.user.email, subject: "Your book, #{@book.title} was reviewed!")
+  	end
+  end
+
+  def notify_about_rating(book_id, stars, rater)
+  	@book = Book.find(book_id)
+  	puts "The book is #{Book.find(book_id).title}"
+  	@stars = stars
+  	@rater = rater
+  	if @book.user.user_profile.mail_pref_rat
+  	  mail(to: @book.user.email, subject: "Your book, #{@book.title} was rated!")
+  	end
   end
 
 end
