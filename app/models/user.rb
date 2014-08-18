77a4e48044d2841ca45dec029f7284c61dcb8a34
Neil Woodward <drlctr@gmail.com>
2014-08-17 19:46:08 -0700

    Changed routing to account for incorporating devise for authentication. [#76106906]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 	validates_presence_of :username
-	# validates :email, format: 
-	#   {with: /\A.+@.+\..+\z/, message:
-	#     "Does not appear to be a valid email address"}
-	validates :password, confirmation:  true, format:  
-	  {with: /\A(?=.*(\d|\W)).{8,20}\z/, message: "requires at least eight
-	   characters, at least one number or non-alphanumeric"}
 
 	has_many :books
 	has_many :reviews
