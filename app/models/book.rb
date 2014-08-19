6b79c8754729670c0df9951d352756189c9a8a22
Neil Woodward <drlctr@gmail.com>
2014-08-18 20:48:21 -0700

    Finished the picture functionality with addition of validation and locating the images in the /public/assets/ folder. [Finishes #76104778]

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,8 +7,13 @@ class Book < ActiveRecord::Base
 	has_attached_file :cover, styles: { small: "150x150>" },
 	  url: "/assets/books/:id/:style/:basename.:extension",
 	  path: ":rails_root/public/assets/books/:id/:style/:basename.:extension"
+	
 	validates_attachment_file_name :cover, matches: 
 	  [/jpe?g\Z/,/png\Z/,/gif\Z/]
+  validates_attachment_presence :cover
+  validates_attachment_size :cover, less_than: 5.megabytes
+
+
 
 	ratyrate_rateable "user_rating"
 	
